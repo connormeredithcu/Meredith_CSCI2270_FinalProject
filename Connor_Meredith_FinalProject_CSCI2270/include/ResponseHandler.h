b1d45db3d9d7727ceb4388d4a55cd59008a57a7a
Connor Meredith <Connor Meredith>
2016-04-25 22:08:47 -0600

    Add comments to the header file explaining the purpose of certain structures

--- Connor_Meredith_FinalProject_CSCI2270/include/ResponseHandler.h
+++ Connor_Meredith_FinalProject_CSCI2270/include/ResponseHandler.h
@@ -6,19 +6,40 @@
 
 using namespace std;
 
+//This struct handles edges in the response graph
 struct ResponseChild;
 
+//This struct contains the word associated to the response node, a vector of successors or children,
+//and a record of the number of times the word has been used. E.g., the instance
+// (key = "DOGS", children = <null>, numTimesUsed = 50)
+//would indicate that the word "DOGS" has been entered 50 times and no word has ever been used
+//directly afterwards.
 struct ResponseNode
 {
-    string name;
+    string key;
     vector<ResponseChild> children;
     double numTimesUsed;
 };
 
+//This structure contains an edge weight and location of a response node. E.g., the bracketed
+//section of the following graph would be a single response child:
+// I[--1->]run
+//This would indicate that the word "run" has been used directly after the word "I" one time.
 struct ResponseChild
 {
     ResponseNode * child;
     double numTimesParented;
+};
+
+//My code uses a BST to organize the response nodes. This is simpler than using a BFS or DFS algorithm
+//in the graph itself.
+struct SearchNode
+{
+    SearchNode * parent;
+    SearchNode * leftChild;
+    SearchNode * rightChild;
+
+    ResponseNode * key;
 };
 
 class ResponseHandler
