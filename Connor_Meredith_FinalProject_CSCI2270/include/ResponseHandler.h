630b8bc8f1b067ac1e3deb518e1ee3e1a3ebd199
Connor Meredith <Connor Meredith>
2016-04-26 20:11:22 -0600

    Add comments to function declarations and reorganize the class header.

--- Connor_Meredith_FinalProject_CSCI2270/include/ResponseHandler.h
+++ Connor_Meredith_FinalProject_CSCI2270/include/ResponseHandler.h
@@ -43,44 +43,52 @@ struct ResponseChild
 //in the graph itself.
 struct SearchNode
 {
+    //Standard BST relationship pointers
     SearchNode * parent;
     SearchNode * leftChild;
     SearchNode * rightChild;
 
+    //The info held by each node in the tree is an address of a Response Node in the graph.
+    //This way, we can perform in-order traversal of the graph and use BST search methods instead of
+    //using Djikstra's algorithm, a BFS, or DFS.
     ResponseNode * key;
 };
 
 class ResponseHandler
 {
     public:
+        //This constructor just handles some basic initialization tasks.
         ResponseHandler();
 
+        //Split a string based on a delimiter into a vector of sections
         vector<string> ParseString(string line, string delim);
-
+        //Add a sentence to the BST and response graph
         void AddSentence (string sentence);
-
-        vector<ResponseChild> firstWords;
-
+        //Given a node, randomly choose a child based on edge weight
         ResponseNode * GetChild(ResponseNode * parent);
+        //Randomly choose a word with which to start a sentence based on usage amount
         ResponseNode * GetFirst();
-
+        //This is a simpler function to find words in the response graph ***That you know exist***
         ResponseNode * FindExistingWord(string word);
-        bool bSTInitialized;
-        vector<string> sentences;
-
+        //Perform an in-order traversal of a subtree of the BST to display vertices, edges, and weights in the graph.
+        void DisplayVerticesEdges(SearchNode * current);
+        //Perform an in-order traversal of the whole BST to display vertices, edges, and weights in the graph.
         void DisplayGraph();
+
+        //Helper flag. This tells whether or not any words have been added to the BST.
+        bool bSTInitialized;
+        //This vector keeps track of the interactions between the program and user during a session.
+        vector<string> sentences;
+        //This vector keeps track of words that are used first in a sentence and their overall number of times used.
+        vector<ResponseChild> firstWords;
     protected:
     private:
-
-
-
+        //This pointer helps when connecting items in the graph. It points to the end_of_sentence RN
         SearchNode * eosHelper;
+        //This points to a special response node. It is the "end of sentence" invisible word.
         ResponseNode * end_of_sentence;
-
+        //This is the root of the BST.
         SearchNode * head;
-        void DisplayVerticesEdges(SearchNode * current);
-
-
 };
 
 #endif // RESPONSEHANDLER_H
